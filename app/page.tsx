22823f6e2375fc964467ee98b5ea79cfc411fe61
Adam Maxwell <maxwell7774@gmail.com>
2024-02-25 19:48:14 -0700

    Updates home page heading

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default async function HomePage() {
     <div className=" h-5/6 flex justify-center">
       <div className="flex flex-col h-full w-full max-w-screen-xl items-center justify-center gap-y-3 text-center">
         <Clipboard className="w-24 h-24" />
-        <h1 className="text-3xl font-semibold">Suggestions and Complaints</h1>
+        <h1 className="text-3xl font-semibold">
+          Suggestions and Complaints {"(S/C)"}
+        </h1>
         <p className="text-lg font-semibold">
           Go here to submit a suggestion or complaint
         </p>
